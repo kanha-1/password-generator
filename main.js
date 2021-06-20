859d037e40b9893f22288c72774bb96cdcb48532
kanha_1 <kanhu.sahoo09@gmail.com>
2021-06-20 22:30:33 +0530

    project added

---
+++ main.js
@@ -0,0 +1,18 @@
+function getPassword() {
+    var chars ="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+<>:{}[]";
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i = 0; i <passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password+=chars.substring(randomNumber,randomNumber + 1);
+    }
+    document.getElementById('password').value = password;
+}
+
+function copyPassword () {
+    var copyPassText=document.getElementById('password')
+    copyPassText.select();
+    copyPassText.setSelectionRange(0,9999);
+    document.execCommand('copy');
+}
